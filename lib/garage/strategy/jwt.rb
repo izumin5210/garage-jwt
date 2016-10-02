0b0e80196544c39e47aeb30903af8ce6d01df18c
izumin5210 <masayuki@izumin.info>
2016-10-02 16:34:54 +0900

    :+1: Extend Garage::Jwt with Utils module and include Utils into Strategy

--- lib/garage/strategy/jwt.rb
+++ lib/garage/strategy/jwt.rb
@@ -2,6 +2,8 @@ module Garage
   module Strategy
     module Jwt
       extend ActiveSupport::Concern
+      include Garage::Jwt::Utils
+
       included do
         before_action :verify_auth, if: -> (_) { verify_permission? }
       end
@@ -15,7 +17,7 @@ module Garage
           @access_token
         else
           token_type, token = request.authorization.try { |h| h.split(/\s+/) }
-          decoded_token = Garage::Jwt::Utils.decode(token, token_type)
+          decoded_token = decode_token(token, token_type)
           if decoded_token.present?
             @access_token = Garage::Strategy::AccessToken.new(decoded_token)
           else
